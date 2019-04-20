dcb22f4b0fb407a587dce5e5d14c8ef9902098d5
Brendon Shea <brendonshea@Brendons-MacBook-Air.local>
2019-04-20 14:12:09 -0400

    First challenge

---
+++ combining-words/code.rb
@@ -0,0 +1,10 @@
+puts "Today we travelled " + "to the planet Mars " + "and danced " + "on the dust " + "while listening " + "to classic music."
+
+var_1 = "Today we travelled"
+var_2 = "to the planet Mars"
+var_3 = "and danced"
+var_4 = "on the dust"
+var_5 = "while listening"
+var_6 = "to classic music"
+
+puts "#{var_1} #{var_2} #{var_3} #{var_4} #{var_5} #{var_6}."
