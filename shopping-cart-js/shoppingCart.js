5bbf69d5f807266c44bde0f29d81b101ddbf6eaa
Brendon Shea <brendonshea@Brendons-MacBook-Air.local>
2019-04-23 23:35:39 -0400

    Shopping Cart JavaScript

---
+++ shopping-cart-js/shoppingCart.js
@@ -0,0 +1,20 @@
+let apples = {
+  itemName: "bag of apples",
+  quantity: 8
+};
+
+let eggs = {
+  itemName: "carton of eggs",
+  quantity: 12
+};
+
+let tuna = {
+  itemName: "cans of tuna",
+  quantity: 4
+};
+
+let shoppingCart = [apples, eggs, tuna];
+
+shoppingCart.forEach((item) => {
+  console.log(`${item.itemName}: ${item.quantity}`)
+})
