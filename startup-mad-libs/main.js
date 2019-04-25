f0d904afea4eed554cf8b0e7696164df991b6cdf
Brendon Shea <brendonshea@Brendons-MacBook-Air.local>
2019-04-25 15:20:58 -0400

    Supplemental-Material

--- startup-mad-libs/main.js
+++ startup-mad-libs/main.js
@@ -4,4 +4,4 @@ let aTargetAudience = 'the world';
 let solveAProblem = 'create new dynamic markets for business';
 let secretSauce = 'by building invite-only Internets';
 
-console.log(`My space exploration program, ${companyName},\nis developing ${anOffering}\nto help ${aTargetAudience}\n${solveAProblem}\n${secretSauce}.`)
+console.log(`My space exploration program, ${companyName},\nis developing ${anOffering}\nto help ${aTargetAudience}\n${solveAProblem}\n${secretSauce}.`);
